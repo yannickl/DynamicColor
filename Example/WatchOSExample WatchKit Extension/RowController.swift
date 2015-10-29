7e061dcdd4994eed0d528fab54ee8571bfe22e6b
Yannick Loriot <yannick.loriot@gmail.com>
2015-10-29 19:43:20 +0100

    [FIX] Travis

---
+++ Example/WatchOSExample WatchKit Extension/RowController.swift
@@ -0,0 +1,15 @@
+//
+//  RowController.swift
+//  WatchOSExample Extension
+//
+//  Created by Yannick LORIOT on 29/10/15.
+//  Copyright © 2015 Yannick LORIOT. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+class RowController: NSObject {
+  @IBOutlet var colorNameLabel: WKInterfaceLabel!
+  @IBOutlet var colorView: WKInterfaceGroup!
+}
---
+++ Example/WatchOSExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchOSExample
+//
+//  Created by Yannick LORIOT on 29/10/15.
+//  Copyright © 2015 Yannick LORIOT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
