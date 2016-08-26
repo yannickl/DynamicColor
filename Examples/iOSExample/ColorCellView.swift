d528bdabc1fd51877cac3ef786e32c069e6fe2e3
Yannick Loriot <yannick.loriot@gmail.com>
2016-08-26 21:21:55 +0200

    Fixing example

--- Examples/iOSExample/ColorCellView.swift
+++ Examples/iOSExample/ColorCellView.swift
@@ -9,14 +9,15 @@
 import UIKit
 
 final class ColorCellView: UICollectionViewCell {
-  @IBOutlet weak var colorView: UIView?
-  @IBOutlet weak var titleLabel: UILabel?
+  @IBOutlet weak var colorView: UIView!
+  @IBOutlet weak var titleLabel: UILabel!
 
   override func layoutSubviews() {
     super.layoutSubviews()
 
-    if let _colorView = colorView {
-      _colorView.layer.cornerRadius = _colorView.bounds.width / 2
-    }
+    // iOS 10 bug: the content view layout is not called when the view is loaded
+    contentView.layoutSubviews()
+
+    colorView.layer.cornerRadius = colorView.bounds.width / 2
   }
 }
